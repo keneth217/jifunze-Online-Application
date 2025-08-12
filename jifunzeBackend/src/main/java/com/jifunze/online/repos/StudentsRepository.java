99861806c77939df3cd50be0dbe2dd82bd108e43
keneth217 <kipyegonkeneth03@gmail.com>
2025-08-12 21:45:22 +0300

    update admin  authenthication and  dashbords

--- jifunzeBackend/src/main/java/com/jifunze/online/repos/StudentsRepository.java
+++ jifunzeBackend/src/main/java/com/jifunze/online/repos/StudentsRepository.java
@@ -8,11 +8,5 @@ import com.jifunze.online.entity.Students;
 
 public interface StudentsRepository extends JpaRepository<Students, Long> {
     Optional<Students> findByPhoneNumber(String phoneNumber);
-    Optional<Students> findByEmail(String email);
-
-
-
     boolean existsByPhoneNumber(String phoneNumber);
-
-    boolean existsByEmail(String email);
 }
